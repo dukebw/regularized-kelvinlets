7e4c20e8ed2c8ceff0d9d7fba3db1b46c111cf67
Brendan Duke <brendanw.duke@gmail.com>
2020-11-23 21:21:16 -0500

    WIP ugh

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "igl/kelvinlets.h"
 #include "igl/opengl/glfw/Viewer.h"
 #include "igl/readOBJ.h"
 #include "igl/unproject.h"
@@ -12,7 +13,80 @@ static Eigen::MatrixXd V_input;
 static Eigen::MatrixXd V_deformed;
 static Eigen::MatrixXi F_input;
 static Eigen::MatrixXi F_deformed;
-static Eigen::MatrixXd result;
+static Eigen::MatrixXd deformed_result;
+/***/
+static Eigen::Vector3d
+kelvinlet_evaluator(double dt, const Eigen::Vector3d x,const Eigen::Vector3d x0, Eigen::Vector3d f)
+{
+        /* Eigen::Vector3d x{s[0], s[1], s[2]}; */
+        /* Eigen::Vector3d x0{s[3], s[4], s[5]}; */
+        /* Eigen::Vector3d f{s[6], s[7], s[8]}; */
+        double brush_radius;
+
+        double poisson_ratio = 0.5;
+        double shear_modulus = 1.0;
+        double a = 1.0/(4.0 * igl::PI * shear_modulus);
+        double b = a/(4.0*(1.0 - poisson_ratio));
+        double c = 2.0/(3.0*a - 2.0*b);
+
+        Eigen::Vector3d r = x - (x0 + (f / c / brush_radius)*dt);
+        double r_norm_sq = r.squaredNorm();
+
+        double r_eps = sqrt(r_norm_sq + brush_radius*brush_radius);
+        double r_eps_3 = r_eps * r_eps * r_eps;
+        Eigen::Vector3d t1 = ((a - b) / r_eps) * f;
+        Eigen::Vector3d t2 = ((b / r_eps_3) * r * r.transpose()) * f;
+        Eigen::Vector3d t3 = ((a * brush_radius * brush_radius) / (2 * r_eps_3)) * f;
+
+        return t1 + t2 + t3;
+        /* Eigen::Vector3d u = t1 + t2 + t3; */
+        /* sd[0] = u(0) - x(0); */
+        /* sd[1] = u(1) - x(1); */
+        /* sd[2] = u(2) - x(2); */
+}
+IGL_INLINE void integrate(const double t,
+                          const double dt,
+                          const Eigen::Vector3d& x,
+                          const Eigen::Vector3d& x0,
+                          const Eigen::Vector3d& f,
+                          Eigen::Vector3d& result,
+                          double& error)
+{
+  constexpr double a1 = 0;
+  constexpr double a2 = 1 / 2.0f;
+  constexpr double a3 = 3 / 4.0f;
+  constexpr double a4 = 1.0f;
+
+  constexpr double b21 = 1 / 2.0f;
+  constexpr double b31 = 0;
+  constexpr double b32 = 3 / 4.0f;
+  constexpr double b41 = 2 / 9.0f;
+  constexpr double b42 = 1 / 3.0f;
+  constexpr double b43 = 4 / 9.0f;
+
+  constexpr double c1 = 2 / 9.0f; // third order answer
+  constexpr double c2 = 1 / 3.0f;
+  constexpr double c3 = 4 / 9.0f;
+
+  constexpr double d1 = 7 / 24.0f; // second order answer
+  constexpr double d2 = 1 / 4.0f;
+  constexpr double d3 = 1 / 3.0f;
+  constexpr double d4 = 1 / 8.0f;
+
+  auto k1 = dt * kelvinlet_evaluator(t + dt * a1, x, x0, f);
+  auto k2 = dt * kelvinlet_evaluator(t + dt * a2, x + k1 * b21, x0, f);
+  auto k3 = dt * kelvinlet_evaluator(
+                   t + dt * a3, x + k1 * b31 + k2 * b32, x0, f);
+  auto k4 =
+    dt * kelvinlet_evaluator(
+           t + dt * a4, x + k1 * b41 + k2 * b42 + k3 * b43, x0, f);
+  auto r1 = x + k1 * d1 + k2 * d2 + k3 * d3 + k4 * d4;
+  auto r2 = x + k1 * c1 + k2 * c2 + k3 * c3;
+  result = r2;
+  error = (r2 - r1).norm() / dt;
+};
+/***/
+
 
 /**
  * NOTE(brendan): Equation 6
@@ -62,27 +136,52 @@ callback_mouse_move(igl::opengl::glfw::Viewer& viewer, int button, int modifie)
 
                 Eigen::Vector3d f = (pos_end - pos_start)*brush_strength;
 
-                double t = 0.0;
-                double dt = 0.1;
-                double t_end = 1.0;
-                for (int i = 0;
-                     i < V_deformed.rows();
-                     ++i) {
-                        asc::RK4 integrator;
-                        RegularizedKelvinlet system;
+                Eigen::Matrix3d mat;
+                mat.setZero();
+                double brush_radius = 1.0;
+                int scale = 1;
+                igl::kelvinlets(V_deformed,
+                                pos_start,
+                                f,
+                                mat,
+                                igl::KelvinletParams<double>(brush_radius, scale, igl::BrushType::GRAB),
+                                deformed_result);
+                /* Eigen::Vector3d result; */
+                /* double error; */
+                /* double t = 0.0; */
+                /* double dt = 0.1; */
+                /* double t_end = 1.0; */
+                /* double safety = 0.9; */
+                /* double max_error = 0.001; */
+                /* for (int i = 0; */
+                /*      i < V_deformed.rows(); */
+                /*      ++i) { */
+                /*         /1* asc::RK4 integrator; *1/ */
+                /*         /1* RegularizedKelvinlet system; *1/ */
 
-                        Eigen::Vector3d x = V_deformed.row(i);
-                        Eigen::Vector3d x0 = pos_start;
-                        asc::state_t s{x(0), x(1), x(2), x0(0), x0(1), x0(2), f(0), f(1), f(2)};
-                        while (t < t_end)
-                                integrator(system, s, t, dt);
+                /*         /1* Eigen::Vector3d x = V_deformed.row(i); *1/ */
+                /*         /1* Eigen::Vector3d x0 = pos_start; *1/ */
+                /*         /1* asc::state_t s{x(0), x(1), x(2), x0(0), x0(1), x0(2), f(0), f(1), f(2)}; *1/ */
+                /*         /1* while (t < t_end) *1/ */
+                /*         /1*         integrator(system, s, t, dt); *1/ */
 
-                        x = {s[0], s[1], s[2]};
-                        result.row(i) = x;
-                }
+                /*         /1* x = {s[0], s[1], s[2]}; *1/ */
+                /*         /1* deformed_result.row(i) = x; *1/ */
 
-                viewer.data().set_mesh(result, F_deformed);
-                viewer.core().align_camera_center(result, F_deformed);
+                /*         dt = std::min(dt, 1 - t); */
+                /*         integrate(t, dt, V_deformed.row(i), pos_start, f, result, error); */
+                /*         auto new_dt = dt * safety * std::pow(max_error / error, 1 / 3.0); */
+                /*         if (error <= max_error || dt <= 0.001) { */
+                /*                 deformed_result.row(i) = result; */
+                /*                 t += dt; */
+                /*                 dt = new_dt; */
+                /*         } else { */
+                /*                 dt = std::max(abs(new_dt - dt) < 0.001 ? dt / 2.f : new_dt, 0.001); */
+                /*         } */
+                /* } */
+
+                viewer.data().set_mesh(deformed_result, F_deformed);
+                viewer.core().align_camera_center(deformed_result, F_deformed);
 
                 return true;
         }
@@ -141,7 +240,7 @@ int main(int argc, char *argv[])
 
         V_deformed = V_input;
         F_deformed = F_input;
-        result.resize(V_deformed.rows(), V_deformed.cols());
+        deformed_result.resize(V_deformed.rows(), V_deformed.cols());
 
         brush_strength = (
                 V_deformed.colwise().maxCoeff() - V_deformed.colwise().minCoeff()
@@ -155,7 +254,7 @@ int main(int argc, char *argv[])
         viewer.callback_mouse_up =
                 [&](igl::opengl::glfw::Viewer& viewer, int, int) -> bool {
                         if (!pos_start.isZero()) {
-                                V_deformed = result;
+                                V_deformed = deformed_result;
                                 pos_start.setZero();
                                 return true;
                         }
